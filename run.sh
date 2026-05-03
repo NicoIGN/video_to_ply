952066f8a6fdc62682b0b56601f7bb8e6cf7fb37
Nicolas Bellaiche <nicolas.bellaiche@ign.fr>
2026-05-03 14:13:11 +0200

    profiles preprocess

--- run.sh
+++ run.sh
@@ -156,6 +156,7 @@ else
   if [ -f "profiles/preprocess/${PREPROCESS_PROFILE}.sh" ]; then
     source profiles/preprocess/${PREPROCESS_PROFILE}.sh
      echo "👉 using profile: preprocess/${PREPROCESS_PROFILE}"
+     echo "MATCHING_METHOD=$MATCHING_METHOD
   else
     echo "❌  profile preprocess/${PREPROCESS_PROFILE} not found"
     echo "❌  use profil hloc | colmap"
