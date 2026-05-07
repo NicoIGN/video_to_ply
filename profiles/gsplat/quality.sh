e90712eb00515a2f07777f4233a75568ed6b6a01
Nicolas Bellaiche <nicolas.bellaiche@ign.fr>
2026-05-07 14:44:42 +0200

    WIP

--- profiles/gsplat/quality.sh
+++ profiles/gsplat/quality.sh
@@ -1,5 +1,5 @@
 ########################################
-# PERFORMANCE PROFILE - QUALITY SAFE
+# PERFORMANCE PROFILE - QUALITY ONLY
 ########################################
 
 TRAINING_PROFILE="gpu/quality"
@@ -13,12 +13,9 @@ TRAIN_VIS_MODE="tensorboard"
 # IMAGE / PREPROCESSING
 ########################################
 
-# montée progressive seulement
-CAMERA_RES_SCALE_FACTOR=1
-MAX_RES=1280
-
+CAMERA_RES_SCALE_FACTOR=1.0
+MAX_RES=1024
 NUM_DOWNSCALES=1
-
 SKIP_IMAGE_PROCESSING=true
 MAX_JOBS=2
 
@@ -26,62 +23,31 @@ MAX_JOBS=2
 # TRAINING
 ########################################
 
-# plus long mais pas extrême
-MAX_ITER=10000
+MAX_ITER=8000
+TRAIN_RAYS_PER_BATCH=512
 
-# améliore gradients sans explosion
-TRAIN_RAYS_PER_BATCH=768
-
-# augmentation modérée
-NUM_NERF_SAMPLES_PER_RAY=32
+NUM_NERF_SAMPLES_PER_RAY=64
 NUM_PROPOSAL_SAMPLES_PER_RAY="128 64"
 
 ########################################
 # GAUSSIAN SPLATTING
 ########################################
 
-# légèrement plus permissif
-DENSIFY_GRAD_THRESH=0.00038
+# densification (plus stable en high-res training)
+DENSIFY_GRAD_THRESH=0.001
 
-# garde davantage de détails
-CULL_ALPHA_THRESH=0.10
+# keep fine structures longer (better thin geometry / edges)
+CULL_ALPHA_THRESH=0.05
 
-# moins agressif mais stable
-CULL_SCREEN_SIZE=0.2
+# slightly more aggressive pruning of oversized splats
+CULL_SCREEN_SIZE=0.25
 
-# split un peu plus fin
+# earlier splitting for higher geometric precision
 SPLIT_SCREEN_SIZE=0.015
-
-########################################
-# DENSIFICATION CONTROL
-########################################
-
-# densification un peu plus active
-REFINE_EVERY=200
-
-# laisse vivre les splits plus longtemps
-STOP_SPLIT_AT=10000
-
-# stabilisation
-RESET_ALPHA_EVERY=40
-
-# nettoyage modéré
-CULL_SCALE_THRESH=0.48
-
-########################################
-# QUALITY / REGULARIZATION
-########################################
-
-USE_BILATERAL_GRID=true
-USE_SCALE_REGULARIZATION=true
-
-MAX_GAUSS_RATIO=10.0
-
-SSIM_LAMBDA=0.25
 
 ########################################
 # EXPORT QUALITY
 ########################################
 
-EXPORT_NUM_POINTS=1500000
+EXPORT_NUM_POINTS=2500000
 EXPORT_DOWNSAMPLE=1
