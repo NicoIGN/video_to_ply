6a2a1474cb3d780e509444aa90f92bb9746bccfc
Nicolas Bellaiche <nicolas.bellaiche@ign.fr>
2026-05-07 13:08:13 +0200

    WIP

--- profiles/gsplat/quality.sh
+++ profiles/gsplat/quality.sh
@@ -14,12 +14,12 @@ TRAIN_VIS_MODE="tensorboard"
 ########################################
 
 # montée progressive seulement
-CAMERA_RES_SCALE_FACTOR=0.9
+CAMERA_RES_SCALE_FACTOR=1
 MAX_RES=1280
 
 # IMPORTANT :
 # garde le downscale stabilisateur
-NUM_DOWNSCALES=1
+NUM_DOWNSCALES=0
 
 SKIP_IMAGE_PROCESSING=true
 MAX_JOBS=2
@@ -36,7 +36,7 @@ TRAIN_RAYS_PER_BATCH=768
 
 # augmentation modérée
 NUM_NERF_SAMPLES_PER_RAY=48
-NUM_PROPOSAL_SAMPLES_PER_RAY="96 48"
+NUM_PROPOSAL_SAMPLES_PER_RAY="128 64"
 
 ########################################
 # GAUSSIAN SPLATTING
@@ -49,20 +49,20 @@ DENSIFY_GRAD_THRESH=0.00038
 CULL_ALPHA_THRESH=0.10
 
 # moins agressif mais stable
-CULL_SCREEN_SIZE=0.22
+CULL_SCREEN_SIZE=0.2
 
 # split un peu plus fin
-SPLIT_SCREEN_SIZE=0.018
+SPLIT_SCREEN_SIZE=0.015
 
 ########################################
 # DENSIFICATION CONTROL
 ########################################
 
 # densification un peu plus active
-REFINE_EVERY=250
+REFINE_EVERY=200
 
 # laisse vivre les splits plus longtemps
-STOP_SPLIT_AT=8500
+STOP_SPLIT_AT=10000
 
 # stabilisation
 RESET_ALPHA_EVERY=40
@@ -77,9 +77,9 @@ CULL_SCALE_THRESH=0.48
 USE_BILATERAL_GRID=true
 USE_SCALE_REGULARIZATION=true
 
-MAX_GAUSS_RATIO=4.0
+MAX_GAUSS_RATIO=10.0
 
-SSIM_LAMBDA=0.26
+SSIM_LAMBDA=0.25
 
 ########################################
 # EXPORT QUALITY
