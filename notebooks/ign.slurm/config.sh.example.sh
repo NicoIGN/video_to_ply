c35aa479117a32ec5698e758078b90a54da6e917
Nicolas Bellaiche <nicolas.bellaiche@ign.fr>
2026-05-11 19:08:39 +0200

    slurm

---
+++ notebooks/ign.slurm/config.sh.example.sh
@@ -0,0 +1,17 @@
+export ROOTDIR="/mnt/common/hdd/home/NBellaiche/data"
+export VIDEOSOURCE="/mnt/common/hdd/home/NBellaiche/data/input/IMG_4848.MOV"
+
+export NUM_FRAMES=200
+
+#branche dev
+export GIT_BRANCH="dev"
+export BASENAME="IGNF2"
+
+
+export PREPROCESS_PROFILE="hloc-lightblue"
+export GSPLAT_PROFILE="balanced"
+
+export HTTP_PROXY=http://proxy.ign.fr:3128
+export HTTPS_PROXY=http://proxy.ign.fr:3128
+export http_proxy=http://proxy.ign.fr:3128
+export https_proxy=http://proxy.ign.fr:3128
---
+++ notebooks/ign.slurm/launch.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+set -e
+
+source config.sh
+
+
+export MPLCONFIGDIR=$HOME_SLURM/.config/matplotlib
+mkdir -p $MPLCONFIGDIR
+
+cd $HOME_SLURM/video_to_ply
+
+echo "===================================="
+echo "🌐 SHELL PROXY ENV"
+echo "HTTP_PROXY  = $HTTP_PROXY"
+echo "HTTPS_PROXY = $HTTPS_PROXY"
+echo "http_proxy  = $http_proxy"
+echo "https_proxy = $https_proxy"
+echo "NO_PROXY    = $NO_PROXY"
+echo "===================================="
+
+python - << 'EOF'
+import os
+
+print("====================================")
+print("🐍 PYTHON ENV PROXY VIEW")
+print("HTTP_PROXY  =", os.getenv("HTTP_PROXY"))
+print("HTTPS_PROXY =", os.getenv("HTTPS_PROXY"))
+print("http_proxy  =", os.getenv("http_proxy"))
+print("https_proxy =", os.getenv("https_proxy"))
+print("NO_PROXY    =", os.getenv("NO_PROXY"))
+print("====================================")
+EOF
+
+
+export HTTP_PROXY
+export HTTPS_PROXY
+export http_proxy
+export https_proxy
+export NO_PROXY
+
+srun \
+  --gres=gpu:1 \
+  --cpus-per-task=4 \
+  --mem=16G \
+  --time=02:00:00 \
+  bash -lc "
+    export HTTP_PROXY=$HTTP_PROXY
+    export HTTPS_PROXY=$HTTPS_PROXY
+    export http_proxy=$http_proxy
+    export https_proxy=$https_proxy
+    export NO_PROXY=$NO_PROXY
+
+    bash run.sh \
+      --root \"$ROOTDIR\" \
+      --name \"$BASENAME\" \
+      --video \"$VIDEOSOURCE\" \
+      --skip-conda \
+      --preprocess-profile \"$PREPROCESS_PROFILE\" \
+      --gsplat-profile \"$GSPLAT_PROFILE\" \
+      --num-frames \"$NUM_FRAMES\"
+  "
