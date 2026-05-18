d99c72a6ef604221e213802dbb02c2ca6bdb0875
Nicolas Bellaiche <nicolas.bellaiche@ign.fr>
2026-05-18 17:30:26 +0200

    test splatfacto-big

--- run.sh
+++ run.sh
@@ -717,6 +717,7 @@ else
         COLLIDER_FAR="$COLLIDER_FAR" \
         ENABLE_COLLIDER="$ENABLE_COLLIDER" \
         USE_BILATERAL_GRID="$USE_BILATERAL_GRID" \
+        USE_DEFAULTS="True" \
         bash scripts/train.sh
 
         print_step_time "TRAINING" "$STEP_START"
